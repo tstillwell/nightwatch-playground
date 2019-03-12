56ac18b3aecc6c6b4d817fd69354e38ccbfe1f5e
tstillwell <tristanstillwell@gmail.com>
2019-03-12 06:18:54 -0600

    feat: Add chained test

---
+++ tests/chained.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'Step1' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+      .end();
+  },
+  'Step2' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+      .end();
+  }
+};
