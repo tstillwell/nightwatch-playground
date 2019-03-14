fd10913f7e114bd3149f75c7a50886e3ac8c04d7
tstillwell <tristanstillwell@gmail.com>
2019-03-13 22:31:26 -0600

    style: Use arrow function for test callback in sample test

--- tests/sample.js
+++ tests/sample.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Demo test Google' : (browser) => {
     browser
       .url('http://www.google.com')
       .waitForElementVisible('body')
