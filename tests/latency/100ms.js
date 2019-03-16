398617ac46ffb8bd133b37977601d6dcb2d2db7e
tstillwell <tristanstillwell@gmail.com>
2019-03-15 22:33:18 -0600

    style: Move 100ms test into 'latency' dir

--- tests/latency/100ms.js
+++ tests/latency/100ms.js
@@ -2,6 +2,6 @@ module.exports = {
   '#main visible' : (browser) => {
     browser
       .url('http://www.google.com')
-      .waitForElementVisible('body',time=100)
+      .waitForElementVisible('body',time=1000)
   }
 };
