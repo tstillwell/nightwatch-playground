f7268c35dd4903bd9e1c0672671496d43d196c77
tstillwell <tristanstillwell@gmail.com>
2019-03-16 22:50:50 -0600

    fix: Adjust 100ms wait period to 100ms instead of 1000ms

--- tests/latency/100ms.js
+++ tests/latency/100ms.js
@@ -2,6 +2,6 @@ module.exports = {
   'body visible in <100ms' : (browser) => {
     browser
       .url('http://www.google.com')
-      .waitForElementVisible('body',time=1000)
+      .waitForElementVisible('body',time=100)
   }
 };
