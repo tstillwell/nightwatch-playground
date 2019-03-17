51799b5086bd08eef554ee0ce92f1653ac7c29cd
tstillwell <tristanstillwell@gmail.com>
2019-03-16 23:24:02 -0600

    feat: Add back test

---
+++ tests/back.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'Click search button' : (browser) => {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+      .pause(1000)
+      .back()
+  }
+};
