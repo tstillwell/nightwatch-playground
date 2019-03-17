005be397f78bf9cf41d0177c06e26825afd748af
tstillwell <tristanstillwell@gmail.com>
2019-03-16 23:21:48 -0600

    feat: Add click test

---
+++ tests/click.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'Click search button' : (browser) => {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+  }
+};
