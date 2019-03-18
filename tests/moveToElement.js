0bd99cb458f9c2bf2def36056c810da216d7d021
tstillwell <tristanstillwell@gmail.com>
2019-03-17 20:35:42 -0600

    feat: Add 'moveToElement' test

---
+++ tests/moveToElement.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'Resize window' : (browser) => {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+      .moveToElement('#navcnt', 0, 0)
+      .pause(1000)
+  }
+};
