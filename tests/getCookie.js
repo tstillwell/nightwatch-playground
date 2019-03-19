b23dc1642aa408eb9d25a298eebe03c953ccb41d
tstillwell <tristanstillwell@gmail.com>
2019-03-18 21:54:47 -0600

    feat: Add 'getCookie' test

---
+++ tests/getCookie.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'Get #main text' : (browser) => {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch js')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+      .pause(1000)
+      .getCookies( cookie => {
+        console.log(cookie);
+      })
+  }
+};
