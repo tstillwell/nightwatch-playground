ebacaf17ee6536e11422d75092a8c6c765530d1f
tstillwell <tristanstillwell@gmail.com>
2019-03-18 21:30:15 -0600

    feat: Add 'getText' test

---
+++ tests/getText.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const outFilePath = './tests/out.txt'
+
+module.exports = {
+  'Output to file' : (browser) => {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch js')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+      .getText('#main', (text) => {
+        console.log(text);
+      })
+  }
+};
